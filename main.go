2d163cd214248970d9f93987d6ad213d7090441d
nikuzuki <nikuzuki29@gmail.com>
2021-01-14 00:17:48 +0900

    go to gin

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"github.com/labstack/echo/v4"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Echo unasuke ")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-	e.GET("/users/:id", getUser)
-	e.GET("/show", show)
-	e.Logger.Fatal(e.Start(":8080"))
-
-	//e.POST("/users", saveUser)
-	//e.GET("/users/:id", getUser)
-	//e.PUT("/users/:id", updateUser)
-	//e.DELETE("/users/:id", deleteUser)
-}
-
-func getUser(c echo.Context) error {
-	// User ID from path `users/:id`
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-}
-
-func show (c echo.Context) error {
-	team := c.QueryParam("team")
-	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
