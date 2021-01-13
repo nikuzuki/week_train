804392b70e9cb15094ccb83c589c23759a5272ec
nikuzuki <nikuzuki29@gmail.com>
2021-01-13 23:49:45 +0900

    とりあえずテンプレート返した

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = renderer
+	e.GET("/hello", Hello)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Echo unasuke ")
 	})
@@ -37,4 +57,8 @@ func save(c echo.Context) error {
 	name := c.FormValue("name")
 	email := c.FormValue("email")
 	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello.html", "World")
 }
