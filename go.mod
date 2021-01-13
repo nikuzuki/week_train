b7b8e9fff18aaa880d8e2aaea1672bb38cf5ba77
nikuzuki <nikuzuki29@gmail.com>
2021-01-13 12:53:35 +0900

    add hotreloadの準備

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module work
 go 1.15
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.17
+	github.com/mattn/echo-livereload v0.0.0-20200327055657-db8a57cc4c02
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	livereload "github.com/mattn/echo-livereload"
+)
 
 func main() {
-	fmt.Println("Hello")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Echo unasuke ")
+	})
+	e.GET("/users/:id", getUser)
+	e.Logger.Fatal(e.Start(":8080"))
+
+	e.Use(middleware.Static("assets"))
+	e.Use(livereload.LiveReload())
+	
+	//e.POST("/users", saveUser)
+	//e.GET("/users/:id", getUser)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+}
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
